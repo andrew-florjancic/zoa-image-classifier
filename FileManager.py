e180575ba1aa4302b1ca092dd23d43fa2c53e47d
Andrew Florjancic <andrewflorjancic@gmail.com>
2024-08-05 17:43:59 -0400

    Add argument parser
    add --train argument
    add --classify argument
    add saved_model argument
    add get_model function to Filemanager module
    add save_model function to Filemanager module
    refactor os.path to use libpath.Path

--- FileManager.py
+++ FileManager.py
@@ -2,12 +2,15 @@
 Basic file manager module to get training and test data.
 """
 import os
+from pathlib import Path
 from skimage.io import imread
 from skimage.transform import resize
 import numpy
+from ClassificationModel import ClassificationModel
+import joblib
 
 
-def get_training_data(path: os.path, image_size: int = None) -> (numpy.ndarray[float], numpy.ndarray[str]):
+def get_training_data(path: Path, image_size: int = None) -> (numpy.ndarray[float], numpy.ndarray[str]):
     """
     Get the images located in the provided directory and return the image data and labels.
     :param path: The path where the training data is located.
@@ -18,14 +21,14 @@ def get_training_data(path: os.path, image_size: int = None) -> (numpy.ndarray[f
     labels = []
     for folder in os.listdir(path):
         for file in os.listdir(os.path.join(path, folder)):
-            image_path = os.path.join(path, folder, file)
+            image_path = Path.joinpath(path, folder, file)
             image_data = get_image_data(path=image_path, size=image_size)
             data.append(image_data)
             labels.append(folder)
     return numpy.asarray(data), numpy.asarray(labels)
 
 
-def get_image_data(path: os.path, size: int = None) -> numpy.ndarray[float]:
+def get_image_data(path: Path, size: int = None) -> numpy.ndarray[float]:
     """
     Get the image from the provided path and return the image data as a flattened array.
     :param path: The path where the image is located.
@@ -36,3 +39,23 @@ def get_image_data(path: os.path, size: int = None) -> numpy.ndarray[float]:
     if size is not None:
         image = resize(image, (size, size))
     return image.flatten()
+
+
+def get_model(path: Path) -> ClassificationModel:
+    """
+    Gets the saved model from the provided location.
+    :param path: The location of the saved model.
+    :return: The saved ClassificationModel object.
+    """
+    return joblib.load(filename=path)
+
+
+def save_model(model: ClassificationModel, path: Path):
+    """
+    Saves the model to the provided location.
+    :param model: The ClassificationModel object to save.
+    :param path:  The location to the saved model.
+    """
+    joblib.dump(value=model, filename=path)
+
+
