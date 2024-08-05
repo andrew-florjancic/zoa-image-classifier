12f3f14dd1c68669dc3026bbb89cbd2f55a7ae5b
Andrew Florjancic <andrewflorjancic@gmail.com>
2024-08-05 17:51:45 -0400

    Add progress indicator
    Create ProgressIndicator module to display loading state information
    Use ProgressIndicator module in the main program

---
+++ ProgressIndicator.py
@@ -0,0 +1,16 @@
+"""
+Basic module to display loading state information to the user
+"""
+
+
+def loading(message: str):
+    """
+    Prints the provided message with a space following the message and no newline
+    :param message: The loading message that will be printed to the console
+    """
+    print(message, end=" ")
+
+
+def done():
+    """Prints a green checkmark indicating the current step is complete"""
+    print('\u2705')
