eb81cb5a01e8cb14a0b0fe72e12d0f40fc4e75bc
Andrew Florjancic <andrewflorjancic@gmail.com>
2024-08-05 17:43:59 -0400

    Add argument parser
    add --train argument
    add --classify argument
    add saved_model argument
    add get_model function to Filemanager module
    add save_model function to Filemanager module
    refactor os.path to use libpath.Path

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
-import os
+import sys
+from pathlib import Path
 import FileManager
 from ClassificationModel import ClassificationModel
+import argparse
 
-# Train the machine learning model with the training data
-training_path = os.path.join(__file__, '..\sample-images\\training-images')
-model = ClassificationModel()
-image_data, image_labels = FileManager.get_training_data(path=training_path, image_size=model.image_size)
-score = model.train(data=image_data, labels=image_labels)
-print(f'Model accuracy: {score:.2%}\n')
+parser = argparse.ArgumentParser("Zoa Image Classifier", description="Basic tool to train and classify images of Zoanthid coral")
+parser.add_argument("--train", type=Path, help="The path of the folder containing the training data")
+parser.add_argument("--classify", type=Path, help="The path of an image to classify")
+parser.add_argument("saved_model", type=Path, help="The path of the saved model to be used for classification, if no model exists one will be saved here")
+args = parser.parse_args()
 
-# Use the trained model to classify one of the sample test images
-test_path = os.path.join(__file__, '..\sample-images\\test-images\\sour_lemons.jpeg')
-image_data = FileManager.get_image_data(path=test_path, size=model.image_size)
-result = model.classify(image_data=image_data)
-print(f'Class probabilities for: {test_path}')
-for prediction in result: print(prediction)
+if args.train is not None:
+    # Train the machine learning model with the training data
+    model = ClassificationModel()
+    print(model.image_size)
+    image_data, image_labels = FileManager.get_training_data(path=args.train, image_size=model.image_size)
+    score = model.train(data=image_data, labels=image_labels)
+    FileManager.save_model(model=model, path=args.saved_model)
+    print(f'Model accuracy: {score:.2%}\n')
+
+
+if args.classify is not None:
+    # Use the trained model to classify one of the sample test images
+    model = FileManager.get_model(path=args.saved_model)
+    if model is None: sys.exit('No saved model found')
+    image_data = FileManager.get_image_data(path=args.classify, size=model.image_size)
+    result = model.classify(image_data=image_data)
+    print(f'Class probabilities for: {args.classify}')
+    for prediction in result: print(prediction)
